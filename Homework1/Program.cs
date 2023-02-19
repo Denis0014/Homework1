04944149946f06a2078f99bb6ea03b9c3be8a35a
Denis <109104246+Denis0014@users.noreply.github.com>
2023-02-19 13:28:16 +0300

    Выполнено задание 1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,9 +4,20 @@ namespace Homework1
 {
     class Program
     {
+        /// <summary>
+        /// Обнуляет разряд десятков числа
+        /// </summary>
+        /// <param name="num">Исходное цисло</param>
+        static void TenToZero(ref int num)
+        {
+            num -= num / 10 % 10 * 10;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Задание 1
+            int a = 145;
+            TenToZero(ref a);
+            Console.WriteLine(a); // 105
         }
     }
 }

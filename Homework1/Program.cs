4874a12cf824844e4d0a9b3da8b71b579b19f9dc
Denis <109104246+Denis0014@users.noreply.github.com>
2023-02-19 13:49:49 +0300

    Выполнил задание 2

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -8,9 +8,22 @@ namespace Homework1
         /// Обнуляет разряд десятков числа
         /// </summary>
         /// <param name="num">Исходное цисло</param>
-        static void TenToZero(ref int num)
+        static void TenToZero(ref int num) => num -= num / 10 % 10 * 10;
+        /// <summary>
+        /// Выводит цвет, которое имеет поле с данными координатами шаматной доски
+        /// </summary>
+        /// <param name="x">Буквенная координата</param>
+        /// <param name="y">Числовая координата</param>
+        /// <returns>Цвет клетки</returns>
+        /// <exception cref="ArgumentException">Выдаёт это исключение, если координаты клетки находятся за границами доски</exception>
+        static string GetChessColor(int x, int y)
         {
-            num -= num / 10 % 10 * 10;
+            if ((x < 1) || (x > 8) || (y < 1) || (y > 8))
+            {
+                throw new ArgumentException("Координаты клетки находятся за границами доски");
+            }
+            if ((x + y) % 2 == 0) return "Чёрный";
+            else return "Белый";
         }
         static void Main(string[] args)
         {
@@ -18,6 +31,8 @@ namespace Homework1
             int a = 145;
             TenToZero(ref a);
             Console.WriteLine(a); // 105
+            // Задание 2
+            Console.WriteLine(GetChessColor(1, 2));
         }
     }
 }
